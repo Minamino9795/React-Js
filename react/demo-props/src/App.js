63595523e2c9198c56561f74f676064208b77804
Minamino9795 <minamino9795@gmail.com>
2023-10-30 09:59:39 +0700

    2

--- react/demo-props/src/App.js
+++ react/demo-props/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Welcome from './components/Welcome';
+import AddComponent from './components/AddComponent';
 
 function App() {
   return (
     <div className="App">
     <Welcome name="Admin" />
+    <AddComponent firstNumber={8} secondNumber={10} />
 </div>
   );
 }
